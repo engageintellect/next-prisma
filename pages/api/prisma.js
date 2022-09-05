7e8f8140179691ec1805df2e7aa4ef2b301294fa
jchadwick <jchadwick@firstam.com>
2022-09-05 11:57:13 -0700

    adding next-prisma + mysql template

--- pages/api/prisma.js
+++ pages/api/prisma.js
@@ -1,9 +1,4 @@
-
-
 import prisma from '../../lib/prisma.ts';
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient()
 
 
 export default async function handler(req, res) {
